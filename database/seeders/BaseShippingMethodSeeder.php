cbb3a050285b1577c3e4356e88b192461fecf946
warmkatahira <t.katahira@warm.co.jp>
2026-02-19 09:58:21 +0900

    20260219_1

--- database/seeders/BaseShippingMethodSeeder.php
+++ database/seeders/BaseShippingMethodSeeder.php
@@ -16,21 +16,21 @@ class BaseShippingMethodSeeder extends Seeder
     {
         BaseShippingMethod::create([
             'shipping_method_id'    => 1,
-            'base_id'               => 'Hiroshima',
+            'base_id'               => 'xxx',
             'setting_1'             => '048997010003',
             'setting_2'             => '01',
             'setting_3'             => 'A',
         ]);
         BaseShippingMethod::create([
             'shipping_method_id'    => 2,
-            'base_id'               => 'Hiroshima',
+            'base_id'               => 'xxx',
             'setting_1'             => '048997010003',
             'setting_2'             => '01',
             'setting_3'             => '8',
         ]);
         BaseShippingMethod::create([
             'shipping_method_id'    => 3,
-            'base_id'               => 'Hiroshima',
+            'base_id'               => 'xxx',
             'setting_1'             => '048997010003',
             'setting_2'             => '01',
             'setting_3'             => '0',
